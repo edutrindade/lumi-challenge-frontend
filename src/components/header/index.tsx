ef8007d55b8908cd391d8d524996d37b0d674ee6
Eduardo Trindade <eduardoepalmeiras@gmail.com>
2024-10-20 02:29:55 -0300

    feat: Criado componente Header

---
+++ src/components/header/index.tsx
@@ -0,0 +1,84 @@
+'use client';
+import { useState } from 'react';
+import { Bell, Filter, Mail, User } from 'lucide-react';
+import { FilterComponent } from '@/components/filter';
+import { Tooltip, TooltipContent, TooltipTrigger } from '@/components/ui/tooltip';
+import { TooltipProvider } from '@radix-ui/react-tooltip';
+
+export const Header = () => {
+    const [isFilterOpen, setIsFilterOpen] = useState(false);
+
+    const classNameButton = isFilterOpen
+        ? 'text-green-500 cursor-pointer hover:text-green-900 mt-1'
+        : 'text-green-900 cursor-pointer hover:text-green-500 mt-1';
+
+    return (
+        <div className='py-2 pr-4 mb-2'>
+            <div className='flex items-center justify-between mb-4'>
+                <h2 className='text-xl font-semibold text-green-900'>Dashboard</h2>
+                <div className='flex items-center gap-6'>
+                    <div onClick={() => setIsFilterOpen(!isFilterOpen)}>
+                        <TooltipProvider>
+                            <Tooltip>
+                                <TooltipTrigger>
+                                    {isFilterOpen ? (
+                                        <div className='relative flex items-center bg-gray-200 rounded-full w-10 h-10 justify-center cursor-pointer'>
+                                            <Filter className={classNameButton} />
+                                        </div>
+                                    ) : (
+                                        <div className='relative flex items-center bg-transparent rounded-full w-10 h-10 justify-center cursor-pointer'>
+                                            <Filter className={classNameButton} />
+                                        </div>
+                                    )}
+                                </TooltipTrigger>
+                                <TooltipContent side='bottom'>
+                                    <span className='text-xs font-semibold text-gray-700'>Filtros</span>
+                                </TooltipContent>
+                            </Tooltip>
+                        </TooltipProvider>
+                    </div>
+
+                    <TooltipProvider>
+                        <Tooltip>
+                            <TooltipTrigger>
+                                <User className='text-green-900 cursor-pointer hover:text-green-500' />
+                            </TooltipTrigger>
+                            <TooltipContent side='bottom'>
+                                <span className='text-xs font-semibold text-gray-700'>Perfil</span>
+                            </TooltipContent>
+                        </Tooltip>
+                    </TooltipProvider>
+
+                    <TooltipProvider>
+                        <Tooltip>
+                            <TooltipTrigger>
+                                <Mail className='text-green-900 cursor-pointer hover:text-green-500' />
+                            </TooltipTrigger>
+                            <TooltipContent side='bottom'>
+                                <span className='text-xs font-semibold text-gray-700'>Caixa de entrada</span>
+                            </TooltipContent>
+                        </Tooltip>
+                    </TooltipProvider>
+
+                    <TooltipProvider>
+                        <Tooltip>
+                            <TooltipTrigger>
+                                <div className='relative flex items-center bg-green-900 rounded-full w-10 h-10 justify-center cursor-pointer'>
+                                    <Bell className='text-white' />
+                                    <span className='absolute top-[-8px] right-[-6px] bg-orange-500 text-white font-bold text-xs rounded-full w-5 h-5 flex items-center justify-center'>
+                                        3
+                                    </span>
+                                </div>
+                            </TooltipTrigger>
+                            <TooltipContent side='bottom'>
+                                <span className='text-xs font-semibold text-gray-700'>Notificações</span>
+                            </TooltipContent>
+                        </Tooltip>
+                    </TooltipProvider>
+                </div>
+            </div>
+
+            {isFilterOpen && <FilterComponent />}
+        </div>
+    );
+};
